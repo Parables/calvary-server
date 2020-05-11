b0ffb897aa867bba62c809488af4f649e68a0f4b
Parables Boltnoel <parables95@gmail.com>
2020-05-11 17:02:36 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  
+</script>
+
+<div class="flex items-center justify-center flex-grow h-screen font-inter">
+  <div class="text-center">
+    <h1 class="text-5xl">
+<strong>Let's start building your app right away </strong>
+     </h1>
+  </div>
+</div>
