b36f9a678b041268fa41bafd5b68b3706ff15cac
Parables Boltnoel <parables95@gmail.com>
2020-05-14 17:06:58 +0000

    moved server to a new branch

--- client/src/routes/Home.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { pop } from 'svelte-spa-router';
-  import WideTable from '../components/WideTable.svelte';
-  import PageTitle from '../components/PageTitle.svelte';
-</script>
-
-<PageTitle title="All Members" isIndex="{true}" />
-<WideTable />
