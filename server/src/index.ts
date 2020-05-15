254d6c61dccde6343d0441596f64703f57806975
Parables Boltnoel <parables95@gmail.com>
2020-05-15 06:17:23 +0000

    Deploy test 6

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,74 +4,79 @@ import { ProfileType, Profile } from './model';
 import connectDB, { allProfiles, createProfile, updateProfile, deleteProfile, profile, search } from './controller'
 
 const init = async () => {
+    try {
 
-    var server = Hapi.server({
-        port: process.env.PORT || 3000,
-        host: 'localhost',
-        routes: {
-            "cors": true
-        }
-    });
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Connected to server';
-        }
-    });
+        var server = Hapi.server({
+            port: process.env.PORT || 3000,
+            host: 'localhost',
+            routes: {
+                "cors": true
+            }
+        });
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return 'Connected to server';
+            }
+        });
 
-    server.route({
-        method: 'GET',
-        path: '/profile',
-        handler: async (request, h) => {
-            const results = await profile(request.query.id)
-            return results;
-        }
-    });
+        server.route({
+            method: 'GET',
+            path: '/profile',
+            handler: async (request, h) => {
+                const results = await profile(request.query.id)
+                return results;
+            }
+        });
 
-    server.route({
-        method: 'GET',
-        path: '/search',
-        handler: async (request, h) => {
-            const results = await search(request.query.name)
-            return results;
-        }
-    });
-    server.route({
-        method: 'GET',
-        path: '/profiles',
-        handler: async (request, h) => {
-            const results = await allProfiles()
-            return results;
-        }
-    });
+        server.route({
+            method: 'GET',
+            path: '/search',
+            handler: async (request, h) => {
+                const results = await search(request.query.name)
+                return results;
+            }
+        });
+        server.route({
+            method: 'GET',
+            path: '/profiles',
+            handler: async (request, h) => {
+                const results = await allProfiles()
+                return results;
+            }
+        });
 
-    server.route({
-        method: ['POST', 'PATCH'],
-        path: '/profile',
-        handler: async (request, h) => {
-            const payload: Profile = JSON.parse(request.payload)
-            const { error, value } = ProfileType.validate(payload);
-            if (error) return error
-            const result = request.method === 'post' ? await createProfile(value) : await updateProfile(value, value.id)
-            return result ? result.toObject() : "No data returned: Error 039";
-        }
-    });
+        server.route({
+            method: ['POST', 'PATCH'],
+            path: '/profile',
+            handler: async (request, h) => {
+                const payload: Profile = JSON.parse(request.payload)
+                const { error, value } = ProfileType.validate(payload);
+                if (error) return error
+                const result = request.method === 'post' ? await createProfile(value) : await updateProfile(value, value.id)
+                return result ? result.toObject() : "No data returned: Error 039";
+            }
+        });
 
-    server.route({
-        method: 'DELETE',
-        path: '/profile',
-        handler: async (request, h) => {
-            const payload: Profile = JSON.parse(request.payload)
-            const { error, value } = ProfileType.validate(payload);
-            if (error) return error
-            const result = await deleteProfile(value.id)
-            return result ? result.toObject() : "No data returned: Error 051";
-        }
-    });
+        server.route({
+            method: 'DELETE',
+            path: '/profile',
+            handler: async (request, h) => {
+                const payload: Profile = JSON.parse(request.payload)
+                const { error, value } = ProfileType.validate(payload);
+                if (error) return error
+                const result = await deleteProfile(value.id)
+                return result ? result.toObject() : "No data returned: Error 051";
+            }
+        });
 
-    await server.start();
-    console.log('🌎 Server running on %s', server.info.uri, server.info.port, typeof server.info.port, process.env.PORT, server);
+        await server.start();
+        console.log('🌎 Server running on %s', server.info.uri, server.info.port, typeof server.info.port, process.env.PORT);
+
+    } catch (error) {
+        console.log("ERror: ", error)
+    }
 };
 
 process.on('unhandledRejection', (err) => {
