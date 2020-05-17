dd40f4965566c7e06d1b4341d44fdf2d84033842
Parables Boltnoel <parables95@gmail.com>
2020-05-17 20:51:55 +0000

    Fixed typo

--- server/src/index.ts
+++ server/src/index.ts
@@ -55,7 +55,7 @@ const init = async () => {
             console.log("PAYLOAD", payload)
             const { error, value } = ProfileType.validate(payload);
             console.log("Logging JOI results: ", value, error)
-            if (error) return error
+            // if (error) return error
             const result = request.method === 'post' ? await createProfile(value) : await updateProfile(value, value.id)
             return result ? result.toObject() : "No data returned: Error 039";
         }
